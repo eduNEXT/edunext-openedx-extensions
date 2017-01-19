0b02b7267f89a22240879b63b0da5a958ffb2f20
Jhony Avella <jhony.avella@edunext.co>
2017-01-19 10:19:57 -0500

    Changing edunext microsite app label to avoid conflicts with open-edx microsite app

--- edunext_openedx_extensions/ednx_microsite_configuration/apps.py
+++ edunext_openedx_extensions/ednx_microsite_configuration/apps.py
@@ -3,5 +3,5 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class AuthTokenConfig(AppConfig):
-    name = 'edunext_openedx_extensions.microsite_configuration'
+    name = 'edunext_openedx_extensions.ednx_microsite_configuration'
     verbose_name = _("Edunext microsite configuration")
