99598574549c29842bdb9f0d06f88922c7ef2bed
Jhony Avella <jhony.avella@edunext.co>
2017-01-19 09:20:32 -0500

    Adding apps config file for microsite_configuration app

---
+++ edunext_openedx_extensions/microsite_configuration/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
+
+
+class AuthTokenConfig(AppConfig):
+    name = 'edunext_openedx_extensions.microsite_configuration'
+    verbose_name = _("Edunext microsite configuration")
