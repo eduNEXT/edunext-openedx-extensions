e1d59d85aa6ebd66c89ee7e2ef85e6b31a4842c3
andrey <andrey.canon@edunext.co>
2018-02-22 10:03:53 -0500

    This commit contains the file command to change the  prod-domain to stage-domain, also includes the files necessary to test this command.
    For example site.test.edunext.co ---->> site-test-edunext-co-stage.edunext.co
    
    	- Create a command to change microsite domain from prod to stage
    	- Amend pep8 and pylint errors
    	- Add log for errors

--- edunext_openedx_extensions/edunext/management/commands/import_xml_db.py
+++ edunext_openedx_extensions/edunext/management/commands/import_xml_db.py
@@ -3,28 +3,46 @@
 """
 Django command to import the users of an external database
 """
-from django.core.management.base import BaseCommand, CommandError
-from xml_importer.database_xml_dump_importer import DatabaseXmlDumpImporter
-from pprint import pformat as pf
 import json
 import logging
+from pprint import pformat as pf
 
-logger = logging.getLogger(__name__)
+from django.core.management.base import BaseCommand, CommandError
+
+try:
+    from xml_importer.database_xml_dump_importer import DatabaseXmlDumpImporter
+except ImportError as detail:
+    print 'ImportError :', detail
+
+LOGGER = logging.getLogger(__name__)  # pylint: disable=no-member
 
 
 class Command(BaseCommand):
+    """
+    Command import_import_xml_db
+    """
     help = 'Imports and Loads the data from an XML dump of a mirosite db'
     USERS_JSON = 'imported_users.json'
     ENROLLMENTS_JSON = 'imported_enrollments.json'
     STUDENTMODULE_JSON = 'imported_studentmodule.json'
 
     def add_arguments(self, parser):
-        parser.add_argument('xmldumpfile', type=str, help='The xml dump of the database that will be imported')
-        parser.add_argument('--users', type=str, nargs='+', required=False, help='List of users (ids) to import')
-        parser.add_argument('--microsite', type=str, required=False, help='Microsite host to add to user')
-        parser.add_argument('--dry-run', action='store_true', required=False, help='Run without saving')
-        parser.add_argument('--keep-user-flags', type=bool, default=False, required=False, help='Keeps special user flags (superuser and staff). Default:False')
-        parser.add_argument('--skip-inactive', type=bool, default=True, required=False, help='Skips inactive users. Default:True')
+        """
+        Add arguments
+        """
+        parser.add_argument('xmldumpfile', type=str,
+                            help='The xml dump of the database that will be imported')
+        parser.add_argument('--users', type=str, nargs='+',
+                            required=False, help='List of users (ids) to import')
+        parser.add_argument('--microsite', type=str,
+                            required=False, help='Microsite host to add to user')
+        parser.add_argument('--dry-run', action='store_true',
+                            required=False, help='Run without saving')
+        parser.add_argument('--keep-user-flags', type=bool, default=False, required=False,
+                            help='Keeps special user flags (superuser and staff). Default:False')
+        parser.add_argument('--skip-inactive', type=bool, default=True,
+                            required=False, help='Skips inactive users. Default:True')
+    # pylint: disable=unused-argument
 
     def handle(self, *args, **options):
         """Entry point for the command.
@@ -53,27 +71,32 @@ class Command(BaseCommand):
                     as_range = range(int(limits[0]), int(limits[1]) + 1)
                     user_ids = user_ids + as_range
                 except Exception:
-                    raise CommandError("Could not process imput {}".format(statement))
+                    raise CommandError(
+                        "Could not process imput {}".format(statement))
 
-        logger.info("Users to import: {}".format(user_ids))
-        logger.info("Microsite: {}".format(microsite))
+        LOGGER.info("Users to import: %s ", user_ids)
+        LOGGER.info("Microsite: %s ", microsite)
 
-        # TODO: if we validate this, we might as well validate it for real with a dom parser or similar
+        # TODO: if we validate this, we might as well validate it for real with
+        # a dom parser or similar
         if not filename.endswith(".xml"):
-            raise CommandError("File {} is not an xml (*.xml)".format(filename))
+            raise CommandError(
+                "File {} is not an xml (*.xml)".format(filename))
 
         importer = DatabaseXmlDumpImporter(filename, dry_run)
 
-        users_imported = importer.import_users(user_ids, keep_special_flags, skip_inactive, microsite)
-        logger.debug("Users Imported:\n" + pf(users_imported, indent=4))
+        users_imported = importer.import_users(
+            user_ids, keep_special_flags, skip_inactive, microsite)
+        LOGGER.debug("Users Imported:\n" + pf(users_imported, indent=4))
         self.save_dic_list(self.USERS_JSON, users_imported)
 
         enrolled = importer.enroll_users(users_imported)
-        logger.debug("Enrollments imported:\n" + pf(enrolled, indent=4))
+        LOGGER.debug("Enrollments imported:\n" + pf(enrolled, indent=4))
         self.save_dic_list(self.ENROLLMENTS_JSON, users_imported)
 
         studentmodules = importer.import_studentmodule(users_imported)
-        logger.debug("Studentmodules imported:\n" + pf(studentmodules, indent=4))
+        LOGGER.debug("Studentmodules imported:\n" +
+                     pf(studentmodules, indent=4))
         self.save_dic_list(self.STUDENTMODULE_JSON, studentmodules)
 
     def save_dic_list(self, filename, new_list):
@@ -85,9 +108,9 @@ class Command(BaseCommand):
         # Load current data
         current_list = []
         try:
-            f = open(filename, 'r')
-            current_list = json.load(f)
-            f.close()
+            data_file = open(filename, 'r')
+            current_list = json.load(data_file)
+            data_file.close()
         except IOError:
             current_list = []
 
@@ -95,5 +118,5 @@ class Command(BaseCommand):
         final_list = current_list + new_list
 
         # Save new data
-        with open(filename, 'w+') as f:
-            json.dump(final_list, f, indent=4)
+        with open(filename, 'w+') as data_file:
+            json.dump(final_list, data_file, indent=4)
