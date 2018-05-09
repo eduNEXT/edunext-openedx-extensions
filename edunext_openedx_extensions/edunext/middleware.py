50924069b523327bb83244f58a5c3674fabc3a1f
Felipe Montoya <felipe.montoya@edunext.co>
2018-05-09 17:42:20 -0500

    Addressing QA

--- edunext_openedx_extensions/edunext/middleware.py
+++ edunext_openedx_extensions/edunext/middleware.py
@@ -9,7 +9,6 @@ from django.contrib.auth.views import redirect_to_login
 from django.http import Http404, HttpResponseRedirect, HttpResponseNotFound
 from django.dispatch import receiver
 from django.db.models.signals import post_save
-from django.shortcuts import resolve_url
 
 import edxmako  # pylint: disable=import-error
 from util.cache import cache  # pylint: disable=import-error
@@ -87,20 +86,12 @@ class PathRedirectionMiddleware(object):
     """
     Middleware to create custom responses based on the request path
     """
-    LOGIN_REQUIRED = "login_required"
-    NOT_FOUND = "not_found"
-    NOT_FOUND_LOGGEDIN = "not_found_loggedin"
-    NOT_FOUND_LOGGEDOUT = "not_found_loggedout"
-    REDIRECT_ALWAYS = "redirect_always"
-    REDIRECT_LOGGEDIN = "redirect_loggedin"
-    REDIRECT_LOGGEDOUT = "redirect_loggedout"
 
     def process_request(self, request):
         """
         This middleware processes the path of every request and determines if there
         is a configured action to take.
         """
-
         if microsite.has_override_value("EDNX_CUSTOM_PATH_REDIRECTS"):
             redirects = microsite.get_value("EDNX_CUSTOM_PATH_REDIRECTS", {})
 
@@ -118,31 +109,60 @@ class PathRedirectionMiddleware(object):
                 ))
 
                 if regex_path_match.match(path):
-                    if key == self.LOGIN_REQUIRED:
-                        if request.user.is_authenticated():
-                            return
-                        else:
-                            resolved_login_url = microsite.get_dict("FEATURES", {}).get("ednx_custom_login_link", settings.LOGIN_URL)
-                            return redirect_to_login(path, resolved_login_url, REDIRECT_FIELD_NAME)
+                    try:
+                        action = getattr(self, key)
+                        return action(request=request, key=key, values=values, path=path)
+                    except AttributeError:
+                        return
 
-                    if key == self.REDIRECT_ALWAYS:
-                        return HttpResponseRedirect(values[key])
+    def login_required(self, request, path, **kwargs):  # pylint: disable=unused-argument
+        """
+        Action: a user session must exist.
+        If it does not, redirect to the login page
+        """
+        if request.user.is_authenticated():
+            return
+        else:
+            resolved_login_url = microsite.get_dict("FEATURES", {}).get(
+                "ednx_custom_login_link", settings.LOGIN_URL)
+            return redirect_to_login(path, resolved_login_url, REDIRECT_FIELD_NAME)
 
-                    if key == self.REDIRECT_LOGGEDIN:
-                        if request.user.is_authenticated():
-                            return HttpResponseRedirect(values[key])
+    def not_found(self, **kwargs):  # pylint: disable=unused-argument
+        """
+        Action: return 404 error for anyone
+        """
+        raise Http404
 
-                    if key == self.REDIRECT_LOGGEDOUT:
-                        if not request.user.is_authenticated():
-                            return HttpResponseRedirect(values[key])
+    def not_found_loggedin(self, request, **kwargs):  # pylint: disable=unused-argument
+        """
+        Action: return 404 error for users which have a session
+        """
+        if request.user.is_authenticated():
+            raise Http404
 
-                    if key == self.NOT_FOUND:
-                        raise Http404
+    def not_found_loggedout(self, request, **kwargs):  # pylint: disable=unused-argument
+        """
+        Action: return 404 error for users that don't have a session
+        """
+        if not request.user.is_authenticated():
+            raise Http404
 
-                    if key == self.NOT_FOUND_LOGGEDIN:
-                        if request.user.is_authenticated():
-                            raise Http404
+    def redirect_always(self, key, values, **kwargs):  # pylint: disable=unused-argument
+        """
+        Action: redirect to the given target
+        """
+        return HttpResponseRedirect(values[key])
 
-                    if key == self.NOT_FOUND_LOGGEDOUT:
-                        if not request.user.is_authenticated():
-                            raise Http404
+    def redirect_loggedin(self, request, key, values, **kwargs):  # pylint: disable=unused-argument
+        """
+        Action: redirect logged users to the given target
+        """
+        if request.user.is_authenticated():
+            return HttpResponseRedirect(values[key])
+
+    def redirect_loggedout(self, request, key, values, **kwargs):  # pylint: disable=unused-argument
+        """
+        Action: redirect external visitors to the given target
+        """
+        if not request.user.is_authenticated():
+            return HttpResponseRedirect(values[key])
