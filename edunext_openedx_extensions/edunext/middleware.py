615356634fc03485a501d490b45191d1ce28a44c
Felipe Montoya <felipe.montoya@edunext.co>
2018-05-09 17:02:56 -0500

    First pass at implementing the redirection feature

--- edunext_openedx_extensions/edunext/middleware.py
+++ edunext_openedx_extensions/edunext/middleware.py
@@ -4,9 +4,12 @@ Middleware for microsite redirections at edunext
 import re
 
 from django.conf import settings
-from django.http import HttpResponseRedirect, HttpResponseNotFound
+from django.contrib.auth import REDIRECT_FIELD_NAME
+from django.contrib.auth.views import redirect_to_login
+from django.http import Http404, HttpResponseRedirect, HttpResponseNotFound
 from django.dispatch import receiver
 from django.db.models.signals import post_save
+from django.shortcuts import resolve_url
 
 import edxmako  # pylint: disable=import-error
 from util.cache import cache  # pylint: disable=import-error
@@ -78,3 +81,68 @@ class MicrositeMiddleware(object):
         """
         cache_key = "redirect_cache." + fasthash(instance.domain)
         cache.delete(cache_key)  # pylint: disable=maybe-no-member
+
+
+class PathRedirectionMiddleware(object):
+    """
+    Middleware to create custom responses based on the request path
+    """
+    LOGIN_REQUIRED = "login_required"
+    NOT_FOUND = "not_found"
+    NOT_FOUND_LOGGEDIN = "not_found_loggedin"
+    NOT_FOUND_LOGGEDOUT = "not_found_loggedout"
+    REDIRECT_ALWAYS = "redirect_always"
+    REDIRECT_LOGGEDIN = "redirect_loggedin"
+    REDIRECT_LOGGEDOUT = "redirect_loggedout"
+
+    def process_request(self, request):
+        """
+        This middleware processes the path of every request and determines if there
+        is a configured action to take.
+        """
+
+        if microsite.has_override_value("EDNX_CUSTOM_PATH_REDIRECTS"):
+            redirects = microsite.get_value("EDNX_CUSTOM_PATH_REDIRECTS", {})
+
+            for regex, values in redirects.iteritems():
+
+                if isinstance(values, dict):
+                    key = next(iter(values))
+                else:
+                    key = values
+
+                path = request.path_info
+                regex_path_match = re.compile(regex.format(
+                    COURSE_ID_PATTERN=settings.COURSE_ID_PATTERN,
+                    USERNAME_PATTERN=settings.USERNAME_PATTERN,
+                ))
+
+                if regex_path_match.match(path):
+                    if key == self.LOGIN_REQUIRED:
+                        if request.user.is_authenticated():
+                            return
+                        else:
+                            resolved_login_url = microsite.get_dict("FEATURES", {}).get("ednx_custom_login_link", settings.LOGIN_URL)
+                            return redirect_to_login(path, resolved_login_url, REDIRECT_FIELD_NAME)
+
+                    if key == self.REDIRECT_ALWAYS:
+                        return HttpResponseRedirect(values[key])
+
+                    if key == self.REDIRECT_LOGGEDIN:
+                        if request.user.is_authenticated():
+                            return HttpResponseRedirect(values[key])
+
+                    if key == self.REDIRECT_LOGGEDOUT:
+                        if not request.user.is_authenticated():
+                            return HttpResponseRedirect(values[key])
+
+                    if key == self.NOT_FOUND:
+                        raise Http404
+
+                    if key == self.NOT_FOUND_LOGGEDIN:
+                        if request.user.is_authenticated():
+                            raise Http404
+
+                    if key == self.NOT_FOUND_LOGGEDOUT:
+                        if not request.user.is_authenticated():
+                            raise Http404
